2fbb10797e3d15153a67748c9abb46b0492d3e9c
vinodvr81 <vinod.vukkalam@gmail.com>
2025-03-04 18:31:52 +0530

    checking git puh from new device

--- wordcountMiniProject/wordcount/views.py
+++ wordcountMiniProject/wordcount/views.py
@@ -8,13 +8,13 @@ def homepage(request):
 
 def word_count(request):
     fulltext = request.GET['subject']
-    countfre = dict()
+    countFrequency = dict()
     words = fulltext.split()
 
     for word in words:
-        if word in countfre:
-            countfre[word] += 1
+        if word in countFrequency:
+            countFrequency[word] += 1
         else:
-            countfre[word] = 1
-    sortedwords = sorted(countfre.items(),key=operator.itemgetter(1),reverse=True)
+            countFrequency[word] = 1
+    sortedwords = sorted(countFrequency.items(),key=operator.itemgetter(1),reverse=True)
     return render(request,"counts.html",{'fulltext': fulltext, 'counttotal':len(words), 'sortedwords':sortedwords})
